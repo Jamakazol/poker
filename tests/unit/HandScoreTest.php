80ad5f69ec63c5aabf1d5c6a92ab1c04b79fa012
Gordon <gordon.henning@gmail.com>
2019-01-21 15:18:00 +0200

    Finishing up score class tests and testing

--- tests/unit/HandScoreTest.php
+++ tests/unit/HandScoreTest.php
@@ -44,174 +44,367 @@ class HandScoreTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals($result, false);
     }
 
-    // public function testIsFourOfAKind()
-    // {
-    //     $handInfo = $this->handInfo;
-    //     $three = false;
-    //     $four = false;
-    //     foreach ($handInfo['values'] as $card => $count) {
-    //         if ($count == 3) {
-    //             $three = true;
-    //         }
-    //         if ($count == 4) {
-    //             $four = true;
-    //         }
-    //     }
-    //     if ($this->joker() == true) {
-    //         if ($three == true) {
-    //             return true;
-    //         } else {
-    //             return false;
-    //         }
-    //     } else {
-    //         if ($four == true) {
-    //             return true;
-    //         } else {
-    //             return false;
-    //         }
-    //     }
-    // }
+    public function testIsFourOfAKind()
+    {
+        $cards = new \PokerApp\Hand('as', 'ks', 'qs', 'js', '10s');
 
-    // public function testIsFullHouse()
-    // {
-    //     $handInfo = $this->handInfo;
-    //     $pairs = $this->countPairs();
+        $three = false;
+        $four = false;
+        foreach ($cards->handInfo['values'] as $card => $count) {
+            if ($count == 3) {
+                $three = true;
+            }
+            if ($count == 4) {
+                $four = true;
+            }
+        }
+        if ($cards->joker() == true) {
+            if ($three == true) {
+                $result = true;
+            } else {
+                $result = false;
+            }
+        } else {
+            if ($four == true) {
+                $result = true;
+            } else {
+                $result = false;
+            }
+        }
 
-    //     if ($this->joker() == true) {
-    //         if ($pairs == 2) {
-    //             return true;
-    //         } else {
-    //             return false;
-    //         }
-    //     } else {
-    //         if (($pairs == 1) && (count($handInfo['values']) == 2)) {
-    //             return true;
-    //         } else {
-    //             return false;
-    //         }
-    //     }
-    // }
+        $this->assertEquals($result, false);
 
-    // public function testIsFlush()
-    // {
-    //     $handInfo = $this->handInfo;
-    //     if (count($handInfo['suits']) == 1) {
-    //         return true;
-    //     } else {
-    //         return false;
-    //     }
-    // }
+        $cards = new \PokerApp\Hand('as', 'ac', 'ad', 'ah', '10s');
 
-    // public function testIsStraight()
-    // {
-    //     $hand = $this->hand;
-    //     $joker = false;
-    //     $handInfo = $this->handInfo;
-    //     $ace = 1;
+        $three = false;
+        $four = false;
+        foreach ($cards->handInfo['values'] as $card => $count) {
+            if ($count == 3) {
+                $three = true;
+            }
+            if ($count == 4) {
+                $four = true;
+            }
+        }
+        if ($cards->joker() == true) {
+            if ($three == true) {
+                $result = true;
+            } else {
+                $result = false;
+            }
+        } else {
+            if ($four == true) {
+                $result = true;
+            } else {
+                $result = false;
+            }
+        }
 
-    //     if (array_key_exists(12, $handInfo['values']) || array_key_exists(13, $handInfo['values'])) {
-    //         $ace = 14;
-    //     }
+        $this->assertEquals($result, true);
+    }
+
+    public function testIsFullHouse()
+    {
+        $cards = new \PokerApp\Hand('as', 'ac', 'ad', '10h', '10s');
+        $pairs = $cards->countPairs();
+
+        if ($cards->joker() == true) {
+            if ($pairs == 2) {
+                $result = true;
+            } else {
+                $result = false;
+            }
+        } else {
+            if (($pairs == 1) && (count($cards->handInfo['values']) == 2)) {
+                $result = true;
+            } else {
+                $result = false;
+            }
+        }
+
+        $this->assertEquals($result, true);
+
+        $cards = new \PokerApp\Hand('as', '9c', 'ad', 'ah', '10s');
+        $pairs = $cards->countPairs();
+
+        if ($cards->joker() == true) {
+            if ($pairs == 2) {
+                $result = true;
+            } else {
+                $result = false;
+            }
+        } else {
+            if (($pairs == 1) && (count($cards->handInfo['values']) == 2)) {
+                $result = true;
+            } else {
+                $result = false;
+            }
+        }
+
+        $this->assertEquals($result, false);
+    }
+
+    public function testIsFlush()
+    {
+        $cards = new \PokerApp\Hand('as', '9s', 'ks', 'ad', '10s');
+
+        if (count($cards->handInfo['suits']) == 1) {
+            $result = true;
+        } else {
+            $result = false;
+        }
+
+        $this->assertEquals($result, false);
+
+        $cards = new \PokerApp\Hand('as', '9s', 'as', 'qs', '10s');
+
+        if (count($cards->handInfo['suits']) == 1) {
+            $result = true;
+        } else {
+            $result = false;
+        }
+
+        $this->assertEquals($result, true);
+    }
+
+    public function testIsStraight()
+    {
+        // False test case
+        $cards = new \PokerApp\Hand('as', '9s', 'ks', 'ad', '10s');
+
+        $hand = $cards->hand;
+        $joker = false;
+        $handInfo = $cards->handInfo;
+        $ace = 1;
+
+        if (array_key_exists(12, $handInfo['values']) || array_key_exists(13, $handInfo['values'])) {
+            $ace = 14;
+        }
         
-    //     if ($this->joker() == true) {
-    //         sort($hand);
-    //         array_pop($hand);
-    //         $joker = true;
-    //     }
+        if ($cards->joker() == true) {
+            sort($hand);
+            array_pop($hand);
+            $joker = true;
+        }
 
-    //     $lastCard = end($hand);
-    //     $firstCard = $hand[0];
+        $lastCard = end($hand);
+        $firstCard = $hand[0];
 
-    //     $index = 0;
-    //     foreach ($hand as $card) {
-    //         $numberValue = (int)substr($card, 0, -1);
-    //         if ($numberValue === 1 && $ace == 14) {
-    //             $hand[$index] = $ace;
-    //         } else {
-    //             $hand[$index] = (int)substr($card, 0, -1);
-    //         }
-    //         $index++;
-    //     }
+        $index = 0;
+        foreach ($hand as $card) {
+            $numberValue = (int)substr($card, 0, -1);
+            if ($numberValue === 1 && $ace == 14) {
+                $hand[$index] = $ace;
+            } else {
+                $hand[$index] = (int)substr($card, 0, -1);
+            }
+            $index++;
+        }
 
-    //     sort($hand);
+        sort($hand);
 
-    //     $straight = true;
+        $result = true;
 
-    //     for ($x = 0; $x < count($hand) - 1; $x++) {
-    //         $card = $hand[$x];
-    //         $nextCard = $hand[$x + 1];
-    //         if ($card + 1 != $nextCard) {
-    //             if ($joker == true) {
-    //                 if ($card + 2 != $nextCard) {
-    //                     $straight = false;
-    //                 }
-    //             } else {
-    //                 $straight = false;
-    //             }
-    //         }
-    //     }
+        for ($x = 0; $x < count($hand) - 1; $x++) {
+            $card = $hand[$x];
+            $nextCard = $hand[$x + 1];
+            if ($card + 1 != $nextCard) {
+                if ($joker == true) {
+                    if ($card + 2 != $nextCard) {
+                        $result = false;
+                    }
+                } else {
+                    $result = false;
+                }
+            }
+        }
 
-    //     return $straight;
-    // }
+        $this->assertEquals($result, false);
 
-    // public function testIsThreeOfAKind()
-    // {
-    //     $handInfo = $this->handInfo;
-    //     $pairs = $this->countPairs();
+        // True test case
+        $cards = new \PokerApp\Hand('as', '10c', 'kh', 'qs', 'jd');
 
-    //     if ($this->joker() == true) {
-    //         if ($pairs == 1) {
-    //             return true;
-    //         } else {
-    //             return false;
-    //         }
-    //     } else {
-    //         if (($pairs == 0) && (count($handInfo['values']) == 3)) {
-    //             return true;
-    //         } else {
-    //             return false;
-    //         }
-    //     }
-    // }
+        $hand = $cards->hand;
+        $joker = false;
+        $handInfo = $cards->handInfo;
+        $ace = 1;
 
-    // public function testIsTwoPair()
-    // {
-    //     $handInfo = $this->handInfo;
-    //     $pairs = $this->countPairs();
+        if (array_key_exists(12, $handInfo['values']) || array_key_exists(13, $handInfo['values'])) {
+            $ace = 14;
+        }
+        
+        if ($cards->joker() == true) {
+            sort($hand);
+            array_pop($hand);
+            $joker = true;
+        }
 
-    //     if ($this->joker() == true) {
-    //         if ($pairs == 1) {
-    //             return true;
-    //         } else {
-    //             return false;
-    //         }
-    //     } else {
-    //         if ($pairs == 2) {
-    //             return true;
-    //         } else {
-    //             return false;
-    //         }
-    //     }
-    // }
+        $lastCard = end($hand);
+        $firstCard = $hand[0];
 
-    // public function testIsOnePair()
-    // {
-    //     $pairs = $this->countPairs();
-    //     $handInfo = $this->handInfo;
+        $index = 0;
+        foreach ($hand as $card) {
+            $numberValue = (int)substr($card, 0, -1);
+            if ($numberValue === 1 && $ace == 14) {
+                $hand[$index] = $ace;
+            } else {
+                $hand[$index] = (int)substr($card, 0, -1);
+            }
+            $index++;
+        }
 
-    //     if ($this->joker() == true) {
-    //         if (count($handInfo['values']) == 4) {
-    //             return true;
-    //         } else {
-    //             return false;
-    //         }
-    //     } else {
-    //         if (($pairs == 1) && (count($handInfo['values']) == 4)) {
-    //             return true;
-    //         } else {
-    //             return false;
-    //         }
-    // }
-      
+        sort($hand);
+
+        $result = true;
+
+        for ($x = 0; $x < count($hand) - 1; $x++) {
+            $card = $hand[$x];
+            $nextCard = $hand[$x + 1];
+            if ($card + 1 != $nextCard) {
+                if ($joker == true) {
+                    if ($card + 2 != $nextCard) {
+                        $result = false;
+                    }
+                } else {
+                    $result = false;
+                }
+            }
+        }
+
+        $this->assertEquals($result, true);
+    }
+
+    public function testIsThreeOfAKind()
+    {
+        $cards = new \PokerApp\Hand('as', 'ad', 'ac', 'qs', 'jd');
+
+        $handInfo = $cards->handInfo;
+        $pairs = $cards->countPairs();
+
+        if ($cards->joker() == true) {
+            if ($pairs == 1) {
+                $result = true;
+            } else {
+                $result = false;
+            }
+        } else {
+            if (($pairs == 0) && (count($handInfo['values']) == 3)) {
+                $result = true;
+            } else {
+                $result = false;
+            }
+        }
+
+        $this->assertEquals($result, true);
+
+        $cards = new \PokerApp\Hand('as', 'ad', 'qc', 'qs', 'jd');
+
+        $handInfo = $cards->handInfo;
+        $pairs = $cards->countPairs();
+
+        if ($cards->joker() == true) {
+            if ($pairs == 1) {
+                $result = true;
+            } else {
+                $result = false;
+            }
+        } else {
+            if (($pairs == 0) && (count($handInfo['values']) == 3)) {
+                $result = true;
+            } else {
+                $result = false;
+            }
+        }
+
+        $this->assertEquals($result, false);
+    }
+
+    public function testIsTwoPair()
+    {
+        $cards = new \PokerApp\Hand('as', 'ad', 'kc', 'ks', 'jd');
+
+        $pairs = $cards->countPairs();
+
+        if ($cards->joker() == true) {
+            if ($pairs == 1) {
+                $result = true;
+            } else {
+                $result = false;
+            }
+        } else {
+            if ($pairs == 2) {
+                $result = true;
+            } else {
+                $result = false;
+            }
+        }
+
+        $this->assertEquals($result, true);
+
+        $cards = new \PokerApp\Hand('as', 'ad', 'ac', 'qs', 'jd');
+
+        $pairs = $cards->countPairs();
+
+        if ($cards->joker() == true) {
+            if ($pairs == 1) {
+                $result = true;
+            } else {
+                $result = false;
+            }
+        } else {
+            if ($pairs == 2) {
+                $result = true;
+            } else {
+                $result = false;
+            }
+        }
+
+        $this->assertEquals($result, false);
+    }
+
+    public function testIsOnePair()
+    {
+        $cards = new \PokerApp\Hand('as', 'ad', 'ac', 'qs', 'jd');
+
+        $pairs = $cards->countPairs();
+        $handInfo = $cards->handInfo;
+        
+        if ($cards->joker() == true) {
+            if (count($handInfo['values']) == 4) {
+                $result = true;
+            } else {
+                $result = false;
+            }
+        } else {
+            if (($pairs == 1) && (count($handInfo['values']) == 4)) {
+                $result = true;
+            } else {
+                $result = false;
+            }
+        }
+
+        $this->assertEquals($result, false);
+
+        $cards = new \PokerApp\Hand('as', 'ad', 'jh', 'qs', '4d');
+
+        $pairs = $cards->countPairs();
+        $handInfo = $cards->handInfo;
+        
+        if ($cards->joker() == true) {
+            if (count($handInfo['values']) == 4) {
+                $result = true;
+            } else {
+                $result = false;
+            }
+        } else {
+            if (($pairs == 1) && (count($handInfo['values']) == 4)) {
+                $result = true;
+            } else {
+                $result = false;
+            }
+        }
+
+        $this->assertEquals($result, true);
+    }
 }
 ?>
